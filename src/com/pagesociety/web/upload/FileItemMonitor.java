1aaf109b98401d767d966dd15689c95f9d7f6397
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2008-09-23 03:00:05 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@900 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/web/upload/FileItemMonitor.java
+++ src/com/pagesociety/web/upload/FileItemMonitor.java
@@ -100,7 +100,6 @@ public class FileItemMonitor extends DiskFileItem
 			bytesRead += b;
 			observer.progress = (((double) (bytesRead)) / observer.fileSize) * 100.0;
 			observer.bytesRead = bytesRead;
-			//observer.complete = (bytesRead == observer.fileSize);//
 			if (MultipartFormConstants.TESTING)
 			{
 				try
