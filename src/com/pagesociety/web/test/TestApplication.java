dc691b4d858921a1d87626224dd7e12e68899fc4
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2008-10-10 00:21:02 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@1069 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/web/test/TestApplication.java
+++ src/com/pagesociety/web/test/TestApplication.java
@@ -1,7 +1,7 @@
 package com.pagesociety.web.test;
 
-import com.pagesociety.web.InitializationException;
 import com.pagesociety.web.WebApplication;
+import com.pagesociety.web.exception.InitializationException;
 
 public class TestApplication extends WebApplication
 {
