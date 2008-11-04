eb0ff81a0ee902a39da8d739c0c19a02c06af247
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2008-11-04 02:35:57 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@1252 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/util/FileInfo.java
+++ src/com/pagesociety/util/FileInfo.java
@@ -17,7 +17,8 @@ public class FileInfo
 	public static final int MPG = 10;
 	public static final int PDF = 11;
 	public static final int DOC = 12;
-	public static final int EXTENSIONS_LENGTH = 13;
+	public static final int SWF = 13;
+	public static final int EXTENSIONS_LENGTH = 14;
 	public static final String[][] EXTENSIONS = new String[EXTENSIONS_LENGTH][];
 	static
 	{
@@ -34,12 +35,14 @@ public class FileInfo
 		EXTENSIONS[MPG]  = new String[] { "mpg", "mpeg", "mp4" };
 		EXTENSIONS[PDF]  = new String[] { "pdf" };
 		EXTENSIONS[DOC]  = new String[] { "doc" };
+		EXTENSIONS[SWF]  = new String[] { "swf" };
 	}
 	// "simple" types
-	public static final int IMAGE    = 100;
-	public static final int AUDIO    = 101;
-	public static final int VIDEO    = 102;
-	public static final int DOCUMENT = 103;
+	public static final int SIMPLE_TYPE_IMAGE    = 100;
+	public static final int SIMPLE_TYPE_AUDIO    = 101;
+	public static final int SIMPLE_TYPE_VIDEO    = 102;
+	public static final int SIMPLE_TYPE_DOCUMENT = 103;
+	public static final int SIMPLE_TYPE_SWF 	 = 104;
 
 	
 	public static int getType(File file)
@@ -70,20 +73,22 @@ public class FileInfo
 		case JPG:
 		case GIF:
 		case PNG:
-			return IMAGE;
+			return SIMPLE_TYPE_IMAGE;
 		case AIF:
 		case MP3:
 		case WAV:
 		case VOG:
-			return AUDIO;
+			return SIMPLE_TYPE_AUDIO;
 		case AVI:
 		case MOV:
 		case DIVX:
 		case MPG:
-			return VIDEO;
+			return SIMPLE_TYPE_VIDEO;
 		case PDF:
 		case DOC:
-			return DOCUMENT;	
+			return SIMPLE_TYPE_DOCUMENT;	
+		case SWF:
+			return SIMPLE_TYPE_SWF;	
 		}
 		return -1;
 	}
@@ -110,6 +115,8 @@ public class FileInfo
 			case PDF:
 			case DOC:
 				return "DOCUMENT";	
+			case SWF:
+				return "SWF";		
 		}
 		return "????";
 	}
@@ -165,6 +172,18 @@ public class FileInfo
 		{
 		case PDF:
 		case DOC:
+			return true;
+		default:
+			return false;
+		}
+	}
+	
+	public static boolean isSWF(File file)
+	{
+		int t = getType(file);
+		switch (t)
+		{
+		case SWF:
 			return true;
 		default:
 			return false;
