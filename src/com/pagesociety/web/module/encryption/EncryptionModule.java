88e3d995e5a1d2300441e1bd7bea35b4f3f0ef95
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2008-11-12 23:49:47 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@1275 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/web/module/encryption/EncryptionModule.java
+++ src/com/pagesociety/web/module/encryption/EncryptionModule.java
@@ -1,9 +1,16 @@
 package com.pagesociety.web.module.encryption;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.security.Security;
 import java.util.Map;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -23,39 +30,118 @@ import com.pagesociety.web.module.WebModule;
 //TODO: add methods for encrypting/decrypting a whole file//
 public class EncryptionModule extends WebModule
 {	
-	String secret_key = "everyone has had a broken heart.";
-	SecretKeySpec key;
-	Cipher cipher;
+	
+	private static final String PARAM_ENCRYPTION_STRENGTH = "encryption-strength";//num of chars used in phrase 32 is 256 bit//
+	private int encryption_strength;
+	private String secret_key;
+	private SecretKeySpec key;
+	private Cipher cipher;
 	public void init(WebApplication app, Map<String,Object> config) throws InitializationException
 	{
 		super.init(app,config);	
-		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());  
-		byte[] key_bytes = secret_key.getBytes();
-		System.out.println("KEY BYTES LENGTH IS "+key_bytes.length);
-		//TODO: have david install unlimited strength encryption policy files
-		//and change back to 32....or make it a configurable parameter or module
-		//along with pass phrase startup stuff..
-		key 		 = new SecretKeySpec(key_bytes,0,16,"AES");
-		try{
-		cipher		 = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
-
-		String enc_f = encryptString("FOOFY");
-		System.out.println("ENCRYPTING STRING FOOFY "+enc_f);
-		System.out.println("DECRYPTING STRING FOOFY "+decryptString(enc_f));
+		encryption_strength = Integer.parseInt(GET_REQUIRED_CONFIG_PARAM(PARAM_ENCRYPTION_STRENGTH, config));
 		
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());  
+		try{
+			cipher		 = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
+			setup_secret_key(app);
 		}catch(Exception e)
 		{
+			e.printStackTrace();
 			throw new InitializationException("FAILED INITIALIZING ENCRYPTION MODULE.COULDNT GET BOUNCEY CASTLE INSTANCE: "+e.getMessage());
 		}
-
-		//app.getConfig().getConfigDirectory();
 	}
 
-	protected void defineSlots()
+	private static final String VERIFY_KEY_DATA_FILENAME = "000";
+	private void setup_secret_key(WebApplication app) throws WebApplicationException,InitializationException
 	{
-		super.defineSlots();
-		//DEFINE_SLOT(SLOT_EMAIL_GUARD,IEmailGuard.class,false,DefaultEmailGuard.class);
+		File key_test_file = GET_MODULE_DATA_FILE(app, VERIFY_KEY_DATA_FILENAME,false);
+		if(key_test_file == null) 
+		{
+			setup_initial_secret_key_v_file(app, key_test_file);
+			return;
+		}
+		
+		try{
+			BufferedReader reader = new BufferedReader(new FileReader(key_test_file));
+			String v_question = reader.readLine();
+			String ev_answer  = reader.readLine();
+		while(true)
+		{
+			String phrase = GET_CONSOLE_INPUT("Enter secret phrase for Encryption Module: "+getName()+">\n>");
+			if(phrase.length() < encryption_strength)
+			{
+					System.out.println("Phrase must be at least "+encryption_strength+" characters long.");
+					continue;
+			}
+			
+			set_secret_key_phrase(phrase);
+			String decrypted_answer;
+			
+			try{
+				decrypted_answer = decryptString(ev_answer);
+			}catch(WebApplicationException bpe)
+			{
+				System.out.println("BAD KEY.");
+				continue;
+			}
+			String response = GET_CONSOLE_INPUT("Does this make sense(Y,N)?\nQ: "+v_question+"\nA: "+decrypted_answer);
+			if(response.equalsIgnoreCase("N"))
+				continue;
+			else
+				break;
+		}	
+		}catch(Exception e)
+		{
+			throw new WebApplicationException(getName()+": PROBLEM READING KEY VERIFICATION FILE.");
+		}
 	}
+	
+	private void setup_initial_secret_key_v_file(WebApplication app,File key_test_file) throws WebApplicationException
+	{
+		String response = GET_CONSOLE_INPUT("Encryption module "+getName()+" needs to be setup. Are you ready?(Y/N)>\n>");
+		if(response.equalsIgnoreCase("N"))
+			throw new WebApplicationException("UNABLE TO START ENCRYPTION MODULE "+getName()+". USER CANCELLED.");
+		
+		String v_question = GET_CONSOLE_INPUT("Provide verification question.>\n>");
+		String v_data     = GET_CONSOLE_INPUT("Provide verification response.>\n>");
+		
+		String secret_phrase;
+		while(true)
+		{
+			secret_phrase = GET_CONSOLE_INPUT("Choose encryption phrase.\n "+"" +
+					"It must be at least "+encryption_strength+" characters long."+
+					"IT WILL BE REQUIRED TO START THE SYSTEM "+
+					"THE NEXT TIME YOU START UP!!!>\n>");
+			if(secret_phrase.length() < encryption_strength)
+				continue;
+			else
+				break;
+		}
+		
+		set_secret_key_phrase(secret_phrase);
+		key_test_file = CREATE_MODULE_DATA_FILE(app, VERIFY_KEY_DATA_FILENAME);
+		try{
+			FileWriter fw = new FileWriter(key_test_file);
+			fw.write(v_question+"\n");
+			fw.write(encryptString(v_data)+"\n");
+			fw.close();
+		}catch(IOException ioe)
+		{
+			ioe.printStackTrace();
+			throw new WebApplicationException("FAILED WRITING SECRET KEY VERIFICATION FILE.");
+		}		
+	}
+	
+
+	private void set_secret_key_phrase(String phrase)
+	{
+		secret_key = phrase;
+		key 		 = new SecretKeySpec(secret_key.getBytes(),0,encryption_strength,"AES");
+	}
+		
+
+
 
 	@Export
 	public String EncryptString(UserApplicationContext uctx,String s) throws PersistenceException,WebApplicationException 
@@ -89,6 +175,7 @@ public class EncryptionModule extends WebModule
 	public String decryptString(String s) throws WebApplicationException
 	{
 		try{ 
+			System.out.println("KEY IS "+secret_key);
 			byte[] cipherText = HexUtil.fromHexString(s);
 			int ctLength = cipherText.length;
 			cipher.init(Cipher.DECRYPT_MODE, key);
@@ -97,7 +184,12 @@ public class EncryptionModule extends WebModule
 			ptLength += cipher.doFinal(plainText, ptLength);
 			//System.out.println("PT LENGTH "+ptLength);
 			return new String(plainText,0,ptLength);
-		}catch(Exception e)
+		}
+		catch(BadPaddingException bpe)
+		{
+			throw new WebApplicationException("BAD PADDING EXCEPTION.PROBABLY BAD KEY");
+		}
+		catch(Exception e)
 		{
 			e.printStackTrace();
 			throw new WebApplicationException("DECRYPTION FAILED DUE TO EXCEPTION. SEE LOGS");
@@ -105,7 +197,4 @@ public class EncryptionModule extends WebModule
 	}
 	
     	
-	
-
-
 }
