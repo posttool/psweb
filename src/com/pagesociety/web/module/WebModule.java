55b440bf1349f765e23a76a14eee7feb16a5b9f1
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2008-11-13 00:06:29 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@1278 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/web/module/WebModule.java
+++ src/com/pagesociety/web/module/WebModule.java
@@ -1,6 +1,13 @@
 package com.pagesociety.web.module;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Map;
 
 import com.pagesociety.persistence.Entity;
@@ -73,6 +80,92 @@ public abstract class WebModule extends Module
 		}
 		
 	}
+	
+	protected  File GET_MODULE_DATA_DIRECTORY(WebApplication app)
+	{
+		File f =  new File(app.getConfig().getWebRootDir()+File.separator+".."+File.separator+"ModuleData"+File.separator+getName()+"Data");
+		if(!f.exists())
+		{
+			System.out.println("CREATING DATA DIRECTORY FOR MODULE: "+getName()+"\n\t"+f.getAbsolutePath());
+			f.mkdirs();
+		}
+		return f;
+	}
+	
+	protected  File GET_MODULE_DATA_FILE(WebApplication app,String filename,boolean create) throws WebApplicationException
+	{
+		File data_dir  =  GET_MODULE_DATA_DIRECTORY(app);
+		File data_file = new File(data_dir,filename);
+		if(!data_file.exists() && create)
+		{
+			System.out.println("CREATING DATA FILE FOR MODULE: "+getName()+"\n\t"+data_file.getAbsolutePath());
+			CREATE_MODULE_DATA_FILE(app,filename);
+		}
+		else if(!data_file.exists() && ! create)
+		{
+			return null;
+		}
+		return data_file;
+	}
 
 	
+	protected  FileReader GET_MODULE_DATA_FILE_READER(WebApplication app,String filename,boolean create) throws WebApplicationException
+	{
+		File data_dir  =  GET_MODULE_DATA_DIRECTORY(app);
+		File data_file = new File(data_dir,filename);
+		if(!data_file.exists() && create)
+		{
+			System.out.println("CREATING DATA FILE FOR MODULE: "+getName()+"\n\t"+data_file.getAbsolutePath());
+			CREATE_MODULE_DATA_FILE(app,filename);
+		}
+		else if(!data_file.exists() && ! create)
+		{
+			return null;
+		}
+		FileReader ret;
+		try{
+			ret =  new FileReader(data_file);
+		}catch(FileNotFoundException fnfe)
+		{
+			throw new WebApplicationException("COULD NOT OPEN READER FOR MODULE DATA FILE "+data_file.getAbsolutePath());
+		}
+		return ret;
+	}
+
+	protected File CREATE_MODULE_DATA_FILE(WebApplication app,String filename) throws WebApplicationException
+	{
+		File data_dir  =  GET_MODULE_DATA_DIRECTORY(app);
+		File data_file = new File(data_dir,filename);
+		try{
+			data_file.createNewFile();
+		}catch(IOException ioe)
+		{
+			throw new WebApplicationException("FAILED CREATING MODULE DATA FILE "+data_file.getAbsolutePath());
+		}
+		
+		return data_file;
+	}
+	
+
+	protected String GET_CONSOLE_INPUT(String prompt)throws WebApplicationException
+	{
+	    if(prompt != null)
+	    {
+	    	System.out.print(prompt);
+	    	System.out.flush();
+	    }
+	    
+	    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	    String input = "";
+
+	    try{
+	        input = in.readLine();
+	       }catch(IOException ioe)
+	        {
+	    	   ioe.printStackTrace();
+	    	   throw new WebApplicationException("ERROR READING CONSOLE INPUT "+ioe.getMessage());
+	        }
+	       
+	   return input;
+	}
 }
