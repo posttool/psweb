a9e0feffaa28ab47637b7c8df3b1d5cbd833a801
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2009-01-10 01:31:50 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@1390 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/web/exception/WebApplicationException.java
+++ src/com/pagesociety/web/exception/WebApplicationException.java
@@ -3,14 +3,32 @@ package com.pagesociety.web.exception;
 public class WebApplicationException extends Exception
 {
 	private static final long serialVersionUID = 1800014981832535683L;
+	
+	private int error_code = 0;
 
-	public WebApplicationException(String message)
-	{
-		super(message);
-	}
 
-	public WebApplicationException(String message, Throwable t)
+	public WebApplicationException(String message, int error_code,Throwable t)
 	{
 		super(message, t);
+		this.error_code = error_code;		
+	}
+	
+	public WebApplicationException(String message, Throwable t)
+	{
+		this(message,0,t);
+	}
+	public WebApplicationException(String message,int error_code)
+	{
+		this(message,error_code,null);
+	}
+	
+	public WebApplicationException(String message)
+	{
+		this(message,0,null);
+	}
+	
+	public int getCode()
+	{
+		return error_code;
 	}
 }
