77abf3b8dc47e7feab73797185021421d816128c
david <david@241bf0be-db3f-0410-a781-e906b5fc445c>
2009-01-27 18:39:14 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@1495 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/web/amf/AmfLong.java
+++ src/com/pagesociety/web/amf/AmfLong.java
@@ -2,48 +2,46 @@ package com.pagesociety.web.amf;
 
 public class AmfLong
 {
-	private int _i;
-	private int _j;
+
+	private String _v;
 
 	public AmfLong()
 	{
 	}
 	
-	public AmfLong(Long value)
+	public AmfLong(Long v)
 	{
-		_i = (int) (value & 0xffffffffL);
-		_j = (int) (value >> 32);
+		boolean ltz = v < 0;
+		if (ltz)
+		{
+			_v = "-"+Long.toHexString(-v);
+		}
+		else
+		{
+			_v = Long.toHexString(v);
+		}
 	}
 
 	public Long longValue()
 	{
-		return (((long) _j) << 32) | (((long) _i) & 0xffffffffL);
+		return Long.parseLong(_v, 16);
 	}
 
-	public int getLow()
+	public String getStringValue()
 	{
-		return _i;
+		return _v;
 	}
-
-	public void setLow(int i)
+	
+	public void setStringValue(String v)
 	{
-		_i = i;
+		_v = v;
 	}
-
-	public int getHigh()
-	{
-		return _j;
-	}
-
-	public void setHigh(int j)
-	{
-		_j = j;
-	}
+	
 
 	public static void main(String[] args)
 	{
-		AmfLong a = new AmfLong(-361873681799100999L);
-		System.out.println(a.longValue());
-		System.out.println(a.getLow() + " " + a.getHigh());
+		AmfLong a = new AmfLong(-3618736817991009L);
+		System.out.println(a.getStringValue()+" "+a.longValue());
+//		System.out.println(a.getLow() + " " + a.getHigh());
 	}
 }
