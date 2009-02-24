e747cd074059b89c0fd40ad419a1eb2373e78de4
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2009-02-24 20:29:21 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@1790 241bf0be-db3f-0410-a781-e906b5fc445c

---
+++ src/com/pagesociety/web/module/util/Validator.java
@@ -0,0 +1,14 @@
+package com.pagesociety.web.module.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator 
+{
+	private static Pattern email_pattern = Pattern.compile("([0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}");
+	public static boolean isValidEmail(String email)
+	{
+		Matcher m =  email_pattern.matcher(email);
+		return m.matches();
+	}
+}
