e4291c159e4d6c05752a53a4fd98c5b7bb7e2afb
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2009-05-07 23:00:32 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@2421 241bf0be-db3f-0410-a781-e906b5fc445c

---
+++ src/com/pagesociety/web/module/TransactionProtect.java
@@ -0,0 +1,9 @@
+package com.pagesociety.web.module;
+
+import java.lang.annotation.Retention;
+
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+public @interface TransactionProtect
+{
+
+}
