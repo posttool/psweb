2ad5e8b94aef0ffa36e05d1d1e700d2abd63c4ed
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2010-01-14 02:13:54 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@3676 241bf0be-db3f-0410-a781-e906b5fc445c

---
+++ src/com/pagesociety/web/module/qa/BugReporterModule.java
@@ -0,0 +1,140 @@
+package com.pagesociety.web.module.qa;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import com.pagesociety.persistence.Entity;
+import com.pagesociety.persistence.PersistenceException;
+import com.pagesociety.persistence.Query;
+import com.pagesociety.persistence.QueryResult;
+import com.pagesociety.persistence.Types;
+import com.pagesociety.util.RandomGUID;
+import com.pagesociety.web.WebApplication;
+import com.pagesociety.web.exception.InitializationException;
+import com.pagesociety.web.module.WebStoreModule;
+import com.pagesociety.web.module.ecommerce.promo.PromotionModule;
+
+
+public class BugReporterModule extends WebStoreModule
+{
+	//this is where images will be uploaded to and served from//
+
+	private File 	bug_image_directory_f 	= null;
+
+	public void init(WebApplication app, Map<String,Object> config) throws InitializationException
+	{
+		super.init(app,config);
+		bug_image_directory_f = new File(GET_MODULE_DATA_DIRECTORY(app),"bug-screenshots");
+		if(!bug_image_directory_f.exists())
+			bug_image_directory_f.mkdirs();
+	}	
+
+	protected void defineSlots()
+	{
+		super.defineSlots();
+		//DEFINE_SLOT(SLOT_EMAIL_MODULE,IEmailModule.class,true);
+	}
+
+	
+	/////////////////BEGIN  M O D U L E   F U N C T I O N S/////////////////////////////////////////
+	public Entity createBug(Entity creator,String submitter,String description,File screenshot) throws PersistenceException
+	{
+			try{
+				START_TRANSACTION();
+				String ext 		= "";
+				String filename = null;
+				if(screenshot != null)
+				{
+					filename = screenshot.getName();
+					int dot_idx = filename.indexOf('.');
+					if( dot_idx != -1)
+						ext = filename.substring(dot_idx).toLowerCase();
+
+					filename = RandomGUID.getGUID()+ext;
+					COPY(screenshot,bug_image_directory_f,filename);
+				}
+				
+				Entity bug = NEW(PS_BUG_ENTITY,
+								creator,
+								PS_BUG_FIELD_SUBMITTER,submitter,
+								PS_BUG_FIELD_DESCRIPTION,description,
+								PS_BUG_FIELD_SCREENSHOT,filename);
+					
+				COMMIT_TRANSACTION();
+				return bug;  
+				
+			}catch(PersistenceException e)
+			{
+				ROLLBACK_TRANSACTION();
+				throw e;
+			}
+	}
+	
+	
+	public List<Entity> getAllBugs() throws PersistenceException
+	{
+		Query q = new Query(PS_BUG_ENTITY);
+		q.idx(Query.PRIMARY_IDX);
+		q.eq(Query.VAL_GLOB);
+		q.orderBy(FIELD_LAST_MODIFIED,Query.DESC);
+		QueryResult results = QUERY(q);
+		return results.getEntities();
+	}
+	
+	public Entity getBugById(long id) throws PersistenceException
+	{
+		return GET(PS_BUG_ENTITY,id);
+	}
+	
+	public void deleteBug(long id) throws PersistenceException
+	{
+		Entity bug = GET(PS_BUG_ENTITY,id);
+		String screenshot = (String)bug.getAttribute(PS_BUG_FIELD_SCREENSHOT);
+		if(screenshot != null)
+		{
+			File ss = new File(bug_image_directory_f,screenshot);
+			ss.delete();
+		}
+		DELETE(bug);
+	}
+	
+	public File getBugScreenshot(long id) throws PersistenceException
+	{
+		Entity bug = GET(PS_BUG_ENTITY,id);;	
+		String ss  = (String)bug.getAttribute(PS_BUG_FIELD_SCREENSHOT);
+		if(ss == null)
+			return null;
+		return new File(bug_image_directory_f,ss);
+	}
+	
+	
+	
+	/////////////////E N D  M O D U L E   F U N C T I O N S/////////////////////////////////////////
+		
+	public static String PS_BUG_ENTITY 	  		   = "PSBug";
+	public static String PS_BUG_FIELD_SUBMITTER    = "title";
+	public static String PS_BUG_FIELD_DESCRIPTION  = "description";
+	public static String PS_BUG_FIELD_SCREENSHOT   = "screenshot_filename";
+
+
+	protected void defineEntities(Map<String,Object> config) throws PersistenceException,InitializationException
+	{
+		DEFINE_ENTITY(PS_BUG_ENTITY,
+				PS_BUG_FIELD_SUBMITTER,Types.TYPE_STRING,null,
+				PS_BUG_FIELD_DESCRIPTION,Types.TYPE_STRING,null,
+				PS_BUG_FIELD_SCREENSHOT,Types.TYPE_STRING,null
+				);
+	
+	}
+
+	//public static final String IDX_BY_USER_BY_PREFERRED = "byUserByPreferred";
+	protected void defineIndexes(Map<String,Object> config) throws PersistenceException,InitializationException
+	{
+		//DEFINE_ENTITY_INDICES
+		//(
+		//		BILLINGRECORD_ENTITY,
+		//		ENTITY_INDEX(IDX_BY_USER_BY_PREFERRED , EntityIndex.TYPE_SIMPLE_MULTI_FIELD_INDEX, FIELD_CREATOR,BILLINGRECORD_FIELD_PREFERRED)
+		//);
+	}
+}
---
+++ src/com/pagesociety/web/module/qa/BugReporterRawUI.java
@@ -0,0 +1,280 @@
+package com.pagesociety.web.module.qa;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.pagesociety.persistence.Entity;
+import com.pagesociety.persistence.PersistenceException;
+import com.pagesociety.web.UserApplicationContext;
+import com.pagesociety.web.WebApplication;
+import com.pagesociety.web.exception.InitializationException;
+import com.pagesociety.web.gateway.RawCommunique;
+import com.pagesociety.web.module.Export;
+import com.pagesociety.web.module.RawUIModule;
+import com.pagesociety.web.module.WebStoreModule;
+import com.pagesociety.web.module.permissions.PermissionEvaluator;
+import com.pagesociety.web.upload.MultipartForm;
+import com.pagesociety.web.upload.MultipartFormException;
+
+
+public class BugReporterRawUI extends RawUIModule
+{
+	private static final String SLOT_BUG_REPORTER_MODULE = "bug-reporter-module";
+	protected BugReporterModule bug_reporter_module;
+
+	
+	public void init(WebApplication app, Map<String,Object> config) throws InitializationException
+	{
+		super.init(app,config);	
+		bug_reporter_module = (BugReporterModule)getSlot(SLOT_BUG_REPORTER_MODULE);
+	}
+
+	protected void defineSlots()
+	{
+		super.defineSlots();
+		DEFINE_SLOT(SLOT_BUG_REPORTER_MODULE,BugReporterModule.class,true);
+	}
+
+	private static final int RAW_SUBMODE_VIEW_BUGS  = 0x01;
+	private static final int RAW_SUBMODE_VIEW_BUG	= 0x02;
+
+	protected void declareSubmodes(WebApplication app,Map<String,Object> config) throws InitializationException
+	{
+		try{
+			declareSubmode(RAW_SUBMODE_DEFAULT,    "submode_default");
+			declareSubmode(RAW_SUBMODE_VIEW_BUGS,  "submode_view_bugs");
+			declareSubmode(RAW_SUBMODE_VIEW_BUG,   "submode_view_bug");
+		}catch(Exception e)
+		{
+			ERROR(e);
+			throw new InitializationException("FAILED BINDING SUBMODE "+e.getMessage());
+		}
+	}
+	
+	
+	@Export
+	public String CreateBug(UserApplicationContext uctx,MultipartForm upload)
+	{
+		StringBuilder buf = new StringBuilder();
+		uctx.setProperty(RawUIModule.KEY_UI_MODULE_OUTPUT_BUF,buf);
+		File tmp = new File(System.getProperty("java.io.tmpdir")+File.separator+"bugzz_img_upload");
+		tmp.mkdir();
+		
+		File f 					= null;
+		FileOutputStream fos 	= null;
+		String upload_filename  = NORMALIZE(upload.getFileName());
+		if(upload_filename != null)
+		{
+			try{
+				f = new File(tmp,upload.getFileName());
+				fos = new FileOutputStream(f);
+				System.out.println("WROTE FILE TO "+f.getAbsolutePath());
+			}catch(Exception e){e.printStackTrace();}
+			try{
+				
+				upload.parse(fos);
+			}catch(MultipartFormException e)
+			{
+				ERROR(e);
+				GOTO_WITH_ERROR(uctx, RAW_SUBMODE_DEFAULT, "Bug Submit ERROR:"+e.getMessage());
+				return buf.toString();
+			}
+		}
+		
+		String submitter 	= upload.getParameter("submitter");
+		String description 	= upload.getParameter("description");
+		try{
+			Entity bug = bug_reporter_module.createBug(null,submitter , description, f);
+			System.out.println("CREATED BUG "+bug);
+		}catch(Exception e)
+		{
+			ERROR(e);
+			GOTO_WITH_ERROR(uctx, RAW_SUBMODE_DEFAULT, "Bug Submit ERROR:"+e.getMessage());
+			return buf.toString();
+		}
+		
+
+		GOTO_WITH_INFO(uctx, RAW_SUBMODE_DEFAULT, "Bug Submit OK. Thank you!");
+		return buf.toString();
+	}
+	
+
+	public void submode_default(UserApplicationContext uctx,Map<String,Object> params)
+	{
+		DOCUMENT_START(uctx, getName(), RAW_UI_BACKGROUND_COLOR, RAW_UI_FONT_FAMILY, RAW_UI_FONT_COLOR, RAW_UI_FONT_SIZE,RAW_UI_LINK_COLOR,RAW_UI_LINK_HOVER_COLOR);
+		P(uctx);
+		SPAN(uctx,"REPORT A BUG",18);
+		DISPLAY_ERROR(uctx,params);
+		DISPLAY_INFO(uctx,params);
+		P(uctx);
+		MULTIPART_FORM_START(uctx,RAW_MODULE_ROOT()+"/"+getName()+"/CreateBug/.form");
+		TABLE_START(uctx,0,800);
+		TR_START(uctx);
+			TD_START(uctx);SPAN(uctx,"Reported By:",16);TD_END(uctx);
+			TD_START(uctx);FORM_INPUT_FIELD(uctx, "submitter", 32, "");TD_END(uctx);
+		TR_END(uctx);
+		TR_START(uctx);
+			TD_START(uctx);SPAN(uctx,"Description:",16);TD_END(uctx);
+			TD_START(uctx);FORM_TEXTAREA_FIELD(uctx, "description", 60, 16);TD_END(uctx);
+		TR_END(uctx);
+			TR_START(uctx);
+				TD_START(uctx);SPAN(uctx,"Screen Shot:",16);TD_END(uctx);
+				TD_START(uctx);FILE_INPUT_FIELD(uctx, "file");TD_END(uctx);
+			TR_END(uctx);
+		TABLE_END(uctx);
+		P(uctx);
+		FORM_SUBMIT_BUTTON(uctx, "+ Create Bug");
+		FORM_END(uctx);
+		HR(uctx);
+		A(uctx,getName(),RAW_SUBMODE_VIEW_BUGS,"[ View Bugs ]");
+		DOCUMENT_END(uctx);
+	}
+	
+	public void submode_view_bugs(UserApplicationContext uctx,Map<String,Object> params)
+	{
+		Entity user = (Entity)uctx.getUser();
+		if(!PermissionEvaluator.IS_ADMIN(user))
+		{
+			CALL_WITH_INFO(uctx,"UserModuleRawUI",RAW_SUBMODE_DEFAULT,RAW_SUBMODE_VIEW_BUGS,"Viewing bugs requires admin login.");
+			return;
+		}
+	try{
+			if(params.get("delete_bid") != null)
+			{
+				long bid =Long.parseLong((String)params.get("delete_bid"));
+				bug_reporter_module.deleteBug(bid);
+			}	
+			DOCUMENT_START(uctx, getName(), RAW_UI_BACKGROUND_COLOR, RAW_UI_FONT_FAMILY, RAW_UI_FONT_COLOR,RAW_UI_FONT_SIZE, 10,RAW_UI_LINK_COLOR,RAW_UI_LINK_HOVER_COLOR);
+			P(uctx);
+			SPAN(uctx,"Bugz =]",18);
+			DISPLAY_ERROR(uctx,params);
+			DISPLAY_INFO(uctx,params);
+			P(uctx);
+			TABLE_START(uctx, 1, 1000);
+			TR_START(uctx);
+			TH(uctx,"Reporter");
+			TH(uctx,"Date");
+			TH(uctx,"Description");
+			TH(uctx,"");
+			TH(uctx,"");
+			TR_END(uctx);
+			List<Entity> bugs = bug_reporter_module.getAllBugs();
+			for(int i = 0;i < bugs.size();i++)
+			{
+				
+				Entity bug = bugs.get(i);
+				String submitter = (String) bug.getAttribute(BugReporterModule.PS_BUG_FIELD_SUBMITTER); 
+				String description = (String) bug.getAttribute(BugReporterModule.PS_BUG_FIELD_DESCRIPTION); 
+				String ss = (String) bug.getAttribute(BugReporterModule.PS_BUG_FIELD_SCREENSHOT); 
+				System.out.println("SCREENSHOT IS "+ss);
+				TR_START(uctx);
+					TD(uctx,submitter);
+					TD(uctx,String.valueOf(bug.getAttribute(WebStoreModule.FIELD_LAST_MODIFIED)));
+					TD(uctx,description);
+					TD_LINK(uctx, getName(), RAW_SUBMODE_VIEW_BUG, "[ view ]", "bid",bug.getId());
+					TD_LINK(uctx, getName(), RAW_SUBMODE_VIEW_BUGS, "[ delete ]", "delete_bid",bug.getId());
+				TR_END(uctx);
+			}
+			
+			TABLE_END(uctx);
+			HR(uctx);
+			A(uctx,getName(),RAW_SUBMODE_DEFAULT,"[ Back ]");
+			DOCUMENT_END(uctx);
+			
+		}catch(Exception e)
+		{
+			ERROR_PAGE(uctx, e);
+		}
+	}
+	
+	public void submode_view_bug(UserApplicationContext uctx,Map<String,Object> params)
+	{
+		Entity user = (Entity)uctx.getUser();
+		if(!PermissionEvaluator.IS_ADMIN(user))
+		{
+			CALL_WITH_INFO(uctx,"UserModuleRawUI",RAW_SUBMODE_DEFAULT,RAW_SUBMODE_VIEW_BUGS,"Coupon promotion manager module requires admin login.");
+			return;
+		}
+		try{
+			long bid =Long.parseLong((String)params.get("bid"));
+			Entity bug = bug_reporter_module.getBugById(bid);
+			String submitter 	= (String)bug.getAttribute(BugReporterModule.PS_BUG_FIELD_SUBMITTER);
+			String description 	= (String)bug.getAttribute(BugReporterModule.PS_BUG_FIELD_DESCRIPTION);
+			String screenshot 	= (String)bug.getAttribute(BugReporterModule.PS_BUG_FIELD_SCREENSHOT);
+			String url = "/BugReporterRawUI/Image/.raw?bid="+String.valueOf(bid);
+			DOCUMENT_START(uctx, getName(), RAW_UI_BACKGROUND_COLOR, RAW_UI_FONT_FAMILY, RAW_UI_FONT_COLOR,RAW_UI_FONT_SIZE, 10,RAW_UI_LINK_COLOR,RAW_UI_LINK_HOVER_COLOR);
+			P(uctx);
+			SPAN(uctx,"Bugz =]",18);
+			DISPLAY_ERROR(uctx,params);
+			DISPLAY_INFO(uctx,params);
+			P(uctx);
+			TABLE_START(uctx,0,1000);
+				TR_START(uctx);
+					TD(uctx,"Submitted By:");TD(uctx,submitter);
+				TR_END(uctx);
+				TR_START(uctx);
+					TD(uctx,"Description:");TD(uctx,description);
+				TR_END(uctx);
+				TR_START(uctx);
+					TD(uctx,"Screen Shot:");TD_START(uctx);
+					if(screenshot!=null)
+						IMG(uctx,url);
+					else
+						NBSP(uctx);
+					TD_END(uctx);
+				TR_END(uctx);
+			TABLE_END(uctx);
+			HR(uctx);
+			A(uctx,getName(),RAW_SUBMODE_VIEW_BUGS,"[ Back ]");
+			DOCUMENT_END(uctx);
+		}catch(Exception e)
+		{
+			ERROR_PAGE(uctx, e);
+		}
+	}
+	
+	@Export
+	public void Image(UserApplicationContext uctx, RawCommunique c) throws PersistenceException
+	{
+		HttpServletRequest request = (HttpServletRequest)c.getRequest();
+		HttpServletResponse response = (HttpServletResponse)c.getResponse();
+		long bid = Long.parseLong(request.getParameter("bid"));
+		File f = bug_reporter_module.getBugScreenshot(bid);
+		if(f == null)
+		{
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			return;
+		}
+		String filename = f.getName();
+		String content_type = null;
+		if(filename.endsWith("jpg") || filename.endsWith("jpeg"))
+			content_type = "image/jpeg";
+		else if(filename.endsWith("png"))
+			content_type = "image/png";
+		else if(filename.endsWith("pic") || filename.endsWith("pict"))
+			content_type = "image/pict";
+		else if(filename.endsWith("gif"))
+			content_type = "image/gif";
+
+        response.setContentType(content_type); /* set the MIME type */
+    	byte[] arBytes = new byte[(int) f.length()];
+    	try{
+    		FileInputStream is= new FileInputStream(f);
+    		is.read(arBytes);
+    		OutputStream os = response.getOutputStream();
+    		os.write(arBytes);
+    		os.flush();
+    	}catch(Exception e)
+    	{
+    		e.printStackTrace();
+    	}
+	}
+	
+}
