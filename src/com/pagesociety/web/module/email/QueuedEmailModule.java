20d5fa0f74b277ef26ee802b0b8157200dfafaa6
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2010-01-22 23:31:03 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@3758 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/web/module/email/QueuedEmailModule.java
+++ src/com/pagesociety/web/module/email/QueuedEmailModule.java
@@ -1,6 +1,7 @@
 package com.pagesociety.web.module.email;
 
 import java.io.File;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +9,9 @@ import java.util.Properties;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.Message;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -19,6 +23,7 @@ import javax.mail.internet.MimeMultipart;
 
 import com.pagesociety.persistence.Entity;
 import com.pagesociety.persistence.PersistenceException;
+import com.pagesociety.web.ApplicationBootstrap;
 import com.pagesociety.web.UserApplicationContext;
 import com.pagesociety.web.WebApplication;
 import com.pagesociety.web.exception.InitializationException;
@@ -34,10 +39,12 @@ public class QueuedEmailModule extends WebModule implements IEmailModule
 	private static final String PARAM_EMAIL_TEMPLATE_DIR  		 = "email-template-dir";
 	private static final String PARAM_EMAIL_RETURN_ADDRESS	     = "email-return-address";
 	private static final String PARAM_EMAIL_QUEUE_SIZE		     = "email-queue-size";
+	private static final String PARAM_EMBEDDED_CONTENT		     = "embedded-content";
 	
 	protected String smtp_server;
 	protected String email_template_dir;
 	protected String email_return_address;
+	protected Object[] embedded_content;
 	
 	protected FreemarkerRenderer fm_renderer;
 
@@ -51,6 +58,7 @@ public class QueuedEmailModule extends WebModule implements IEmailModule
 		smtp_server 	     = GET_REQUIRED_CONFIG_PARAM(PARAM_SMTP_SERVER, config);
 		email_template_dir   = GET_REQUIRED_CONFIG_PARAM(PARAM_EMAIL_TEMPLATE_DIR, config);
 		email_return_address = GET_REQUIRED_CONFIG_PARAM(PARAM_EMAIL_RETURN_ADDRESS, config);
+		setup_embedded_content(config);
 		
 		File f = new File(email_template_dir);
 		if(!f.exists())
@@ -62,6 +70,40 @@ public class QueuedEmailModule extends WebModule implements IEmailModule
 			email_queue_size = Integer.parseInt((String)config.get(PARAM_EMAIL_QUEUE_SIZE));
 		email_queue         = new ArrayBlockingQueue<queue_obj>(email_queue_size);
 		
+	}
+	
+	private void setup_embedded_content(Map<String,Object> config) throws InitializationException
+	{
+		String[] embedded_content_string = GET_OPTIONAL_LIST_PARAM(PARAM_EMBEDDED_CONTENT, config);
+		if(embedded_content_string == null)
+			embedded_content = new Object[0];
+		if(embedded_content_string.length % 3 != 0)
+			throw new InitializationException("embedded_content param should have a length which is a multiple of 3 since format for each item is <embed-content-id>,<embed-mime-type>,<embed-filename>");
+		try{
+			File web_root = new File(getApplication().getConfig().getWebRootDir());
+			embedded_content = new Object[embedded_content_string.length];
+			for(int i = 0; i < embedded_content_string.length;i+=3)
+			{
+					
+				String cid 		 = embedded_content_string[i];
+				String mime_type = embedded_content_string[i+1];
+				String filename  = embedded_content_string[i+2];
+				//check web root than absolutepath//
+				File content_file = new File(web_root,filename);
+				if(!content_file.exists())
+				{
+					content_file = new File(filename);
+					if(content_file == null)
+						throw new InitializationException("COULDN'T FIND "+filename);
+				}
+				embedded_content[i]   = cid;
+				embedded_content[i+1] = mime_type;
+				embedded_content[i+2] = content_file;
+			}
+		}catch(Exception e)
+		{
+			throw new InitializationException("Failed parsing embedded content param. "+e.getMessage());
+		}
 	}
 	
 	public void loadbang(WebApplication app, Map<String,Object> config) throws InitializationException
@@ -122,7 +164,7 @@ public class QueuedEmailModule extends WebModule implements IEmailModule
 		qo.subject = subject;
 		qo.template_name = template_name;
 		qo.template_data = template_data;
-
+		
 		try{
 			email_queue.put(qo);
 		}catch(InterruptedException ie)
@@ -133,6 +175,28 @@ public class QueuedEmailModule extends WebModule implements IEmailModule
 	}
 	//// END MODULE STUFF ///
 	
+	private MimeMultipart embed_content(MimeMultipart multipart) throws Exception
+	{
+		
+		for(int i = 0;i < embedded_content.length;i+=3)
+		{
+			String cid	 	 = (String)embedded_content[i];
+			String mime_type = (String)embedded_content[i+1];
+			File   content   = (File)embedded_content[i+2];
+	
+			MimeBodyPart messageBodyPart = new MimeBodyPart();
+
+			// Fetch the image and associate to part
+			DataSource fds = new FileDataSource(content);
+			messageBodyPart.setDataHandler(new DataHandler(fds));
+			messageBodyPart.setFileName(content.getName());
+			messageBodyPart.setHeader("Content-ID","<"+cid+">");
+
+			// Add part to multi-part
+			multipart.addBodyPart(messageBodyPart);
+		}
+		return multipart;
+	}
 	
 	private void do_send_mail(String from, String[] to, String subject,String template_name,Map<String,Object> template_data) throws WebApplicationException
 	{
@@ -230,20 +294,57 @@ public class QueuedEmailModule extends WebModule implements IEmailModule
 		    String PLAINTEXTbody =  fm_renderer.render(plaintext_template_name, template_data);
 		    String HTMLbody =  fm_renderer.render(template_name, template_data);
 		    
+		    //This is how it would start if we nedded to do attachements
+		    //MimeMultipart container = new MimeMultipart("mixed");
+		    //mixed
+		    //	alternative
+		    //		text
+		    //		related
+		    //			html
+		    //			embedded
+		    //	attachment 1
+		    //	attachment 2
+		    // see http://forums.sun.com/thread.jspa?threadID=5372287
 
+		    
 		    MimeMultipart content = new MimeMultipart("alternative");
-		    MimeBodyPart text = new MimeBodyPart();
-		    MimeBodyPart html = new MimeBodyPart();
-		    text.setText( PLAINTEXTbody);
-		    text.setHeader("MIME-Version" , "1.0" );
-		    text.setHeader("Content-Type" , text.getContentType() );
+		    	MimeBodyPart text = new MimeBodyPart();
+			    text.setText( PLAINTEXTbody);
+			    text.setHeader("MIME-Version" , "1.0" );
+			    text.setHeader("Content-Type" , text.getContentType() );
 		    content.addBodyPart(text);
 		    
-		    html.setContent(HTMLbody, "text/html");
-		    html.setHeader("MIME-Version" , "1.0" );
-		    html.setHeader("Content-Type" , "text/html" );
-		    content.addBodyPart(html);
+		    	MimeMultipart html_multipart = new MimeMultipart("related");
+				    MimeBodyPart html = new MimeBodyPart();
+				    html.setContent(HTMLbody, "text/html");
+				    html.setHeader("MIME-Version" , "1.0" );
+				    html.setHeader("Content-Type" , "text/html" );
+				    html_multipart.addBodyPart(html);
+				    /* this is the stuff from the config file if specified*/
+				    for(int i = 0;i < embedded_content.length;i+=3)
+					{
+						String cid	 	 = (String)embedded_content[i];
+						String mime_type = (String)embedded_content[i+1];
+						File   embed_content   = (File)embedded_content[i+2];
+				
+						MimeBodyPart embed_body_part = new MimeBodyPart();
 
+						// Fetch the image and associate to part
+						DataSource fds = new FileDataSource(embed_content);
+						embed_body_part.setDataHandler(new DataHandler(fds));
+						embed_body_part.setFileName(embed_content.getName());
+						embed_body_part.setHeader("Content-Type",mime_type);
+						embed_body_part.setHeader("Content-ID","<"+cid+">");
+						embed_body_part.setHeader("Content-Disposition","inline");
+						// Add part to multi-part
+						html_multipart.addBodyPart(embed_body_part);
+					}
+		    
+		    MimeBodyPart related_bodypart = new MimeBodyPart();
+		    related_bodypart.setContent(html_multipart);
+		    content.addBodyPart(related_bodypart);
+		    
+		    
 		    // Setting the Subject and Content Type
 		    msg.setContent( content );
 		    msg.setHeader("MIME-Version" , "1.0" );
