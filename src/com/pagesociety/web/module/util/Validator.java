9a35da613557aa4c0277ab06efaf17b2b34d6aee
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2010-02-05 21:50:36 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@3873 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/web/module/util/Validator.java
+++ src/com/pagesociety/web/module/util/Validator.java
@@ -12,6 +12,27 @@ public class Validator
 		return m.matches();
 	}
 	
+	private static Pattern alpha_pattern = Pattern.compile("([a-zA-Z]+");
+	public static boolean isAlpha(String s)
+	{
+		Matcher m =  email_pattern.matcher(s);
+		return m.matches();
+	}
+	
+	private static Pattern alphanumeric_pattern = Pattern.compile("([0-9a-zA-Z]+");
+	public static boolean isAlphaNumeric(String s)
+	{
+		Matcher m =  alphanumeric_pattern.matcher(s);
+		return m.matches();
+	}
+	
+	private static Pattern numeric_pattern = Pattern.compile("([0-9]+");
+	public static boolean isNumeric(String s)
+	{
+		Matcher m =  email_pattern.matcher(s);
+		return m.matches();
+	}
+	
 	public static boolean isEmptyOrNull(String s) 
 	{
 		if(s== null)
