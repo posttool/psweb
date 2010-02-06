c8abf3cb6a3756fb6b719bd5b7e0103c5c7a4a47
david <david@241bf0be-db3f-0410-a781-e906b5fc445c>
2010-02-06 00:16:11 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@3882 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/web/module/util/Validator.java
+++ src/com/pagesociety/web/module/util/Validator.java
@@ -12,24 +12,24 @@ public class Validator
 		return m.matches();
 	}
 	
-	private static Pattern alpha_pattern = Pattern.compile("([a-zA-Z]+");
+	private static Pattern alpha_pattern = Pattern.compile("[a-zA-Z]+");
 	public static boolean isAlpha(String s)
 	{
-		Matcher m =  email_pattern.matcher(s);
+		Matcher m =  alpha_pattern.matcher(s);
 		return m.matches();
 	}
 	
-	private static Pattern alphanumeric_pattern = Pattern.compile("([0-9a-zA-Z]+");
+	private static Pattern alphanumeric_pattern = Pattern.compile("[0-9a-zA-Z]+");
 	public static boolean isAlphaNumeric(String s)
 	{
 		Matcher m =  alphanumeric_pattern.matcher(s);
 		return m.matches();
 	}
 	
-	private static Pattern numeric_pattern = Pattern.compile("([0-9]+");
+	private static Pattern numeric_pattern = Pattern.compile("[0-9]+");
 	public static boolean isNumeric(String s)
 	{
-		Matcher m =  email_pattern.matcher(s);
+		Matcher m =  numeric_pattern.matcher(s);
 		return m.matches();
 	}
 	
