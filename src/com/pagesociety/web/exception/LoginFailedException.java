f720572f34d9ef548711749deb5281795faa8359
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2010-03-24 19:50:46 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@4209 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/web/exception/LoginFailedException.java
+++ src/com/pagesociety/web/exception/LoginFailedException.java
@@ -10,6 +10,11 @@ public class LoginFailedException extends WebApplicationException {
 
 	}
 
+	public LoginFailedException(String message,int code) {
+		super(message,code);
+
+	}
+
 	public LoginFailedException(String message, Throwable t) {
 		super(message, t);
 	}
