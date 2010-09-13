efa3a1dc3635de82549b2b70f95015ca42c92da8
david <david@241bf0be-db3f-0410-a781-e906b5fc445c>
2010-09-13 21:14:48 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@5507 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/web/module/application/ApplicationInfoModule.java
+++ src/com/pagesociety/web/module/application/ApplicationInfoModule.java
@@ -1,31 +1,16 @@
 package com.pagesociety.web.module.application;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-import com.pagesociety.persistence.Entity;
-import com.pagesociety.persistence.EntityDefinition;
-import com.pagesociety.persistence.EntityIndex;
-import com.pagesociety.persistence.FieldDefinition;
-import com.pagesociety.persistence.PersistenceException;
-import com.pagesociety.persistence.Query;
 import com.pagesociety.web.UserApplicationContext;
 import com.pagesociety.web.WebApplication;
 import com.pagesociety.web.exception.InitializationException;
 import com.pagesociety.web.exception.WebApplicationException;
 import com.pagesociety.web.module.Export;
 import com.pagesociety.web.module.Module;
-import com.pagesociety.web.module.PagingQueryResult;
 import com.pagesociety.web.module.WebModule;
-import com.pagesociety.web.module.WebStoreModule;
-import com.pagesociety.web.module.WebModule.OBJECT;
-import com.pagesociety.web.module.permissions.DefaultPermissionsModule;
-import com.pagesociety.web.module.permissions.PermissionsModule;
 import com.pagesociety.web.module.resource.ResourceModule;
 
 public class ApplicationInfoModule extends WebModule
