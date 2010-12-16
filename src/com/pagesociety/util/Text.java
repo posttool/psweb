015b24aabd91e0c9f2b1dd14adc83d7e1d069409
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2010-12-16 19:14:24 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@5989 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/util/Text.java
+++ src/com/pagesociety/util/Text.java
@@ -288,6 +288,8 @@ public class Text
 		return b.toString();
 	}
 
+	
+	
 	// ///
 	public static String[][] MONTHS = { { "1", "Jan" }, { "2", "Feb" }, { "3", "Mar" }, { "4", "Apr" }, { "5", "May" }, { "6", "Jun" }, { "7", "Jul" }, { "8", "Aug" }, { "9", "Sep" }, { "10", "Oct" }, { "11", "Nov" }, { "12", "Dec" } };
 	//
