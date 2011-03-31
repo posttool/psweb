9787f8cb8526bd4a347195c51dca94474a9ae405
david <david@241bf0be-db3f-0410-a781-e906b5fc445c>
2011-03-31 21:39:53 +0000

    new optional param: max-days-to-keep-backup (defaults to 14)
    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@6710 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/web/module/persistence/PersistenceBackupManagerRawUI.java
+++ src/com/pagesociety/web/module/persistence/PersistenceBackupManagerRawUI.java
@@ -3,17 +3,26 @@ package com.pagesociety.web.module.persistence;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.Serializable;
+import java.text.Format;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 
 import javax.servlet.http.HttpServletResponse;
@@ -21,9 +30,11 @@ import javax.servlet.http.HttpServletResponse;
 
 
 import com.pagesociety.persistence.Entity;
+import com.pagesociety.persistence.PersistenceException;
 import com.pagesociety.web.UserApplicationContext;
 import com.pagesociety.web.WebApplication;
 import com.pagesociety.web.exception.InitializationException;
+import com.pagesociety.web.exception.WebApplicationException;
 import com.pagesociety.web.module.RawUIModule;
 import com.pagesociety.web.module.permissions.PermissionEvaluator;
 
@@ -33,27 +44,40 @@ public class PersistenceBackupManagerRawUI extends RawUIModule
 {	
 	private static final String SLOT_STORE = "store";
 	private static final String PARAM_FULL_BACKUP_TIME 		  		= "full-backup-time";
+	private static final String PARAM_MAX_DAYS		     	  		= "max-days-to-keep-backup";
 	private static final String PARAM_INCREMENTAL_BACKUP_INTERVAL 	= "incremental-backup-interval";
 	
 	protected IPersistenceProvider pp;
 	
 	private File data_file;
-	private Map<String,String> last_backup_map;
+	private SortedMap<String,BackupInfo> last_backup_map;
 	private int backup_time_hr;
 	private int backup_time_min;
 	private int inc_backup_interval;//TODO
+	private int max_days;
 
 	
 	public void init(WebApplication app, Map<String,Object> config) throws InitializationException
 	{
 		super.init(app,config);	
 		pp = (IPersistenceProvider)getSlot(SLOT_STORE);
-		try{
-			init_last_backup_map();
-		}catch(Exception e)
+		try
 		{
-			throw new InitializationException(e.getMessage());
+			init_last_backup_map();
+		} catch (Exception e)
+		{
+			throw new InitializationException("Couldnt read init backup map",e);
 		}
+		
+		max_days = GET_OPTIONAL_INT_CONFIG_PARAM(PARAM_MAX_DAYS, 14, config);
+		try
+		{
+			remove_old_backups();
+		} catch (Exception e)
+		{
+			throw new InitializationException("Couldnt remove old backups",e);
+		}
+		
 		String[] backup_time	= GET_OPTIONAL_LIST_PARAM(PARAM_FULL_BACKUP_TIME, config);
 		if (backup_time != null)
 		{
@@ -66,19 +90,95 @@ public class PersistenceBackupManagerRawUI extends RawUIModule
 		}
 	}
 
-	private void init_last_backup_map() throws Exception
+	private void init_last_backup_map() throws WebApplicationException, IOException, PersistenceException 
 	{
 		data_file = GET_MODULE_DATA_FILE(getApplication(), "last_backup_map", true);
 		try{
 			read_backup_map();
 		}catch(Exception e)
 		{
-			last_backup_map = new HashMap<String, String>();
+			last_backup_map = new TreeMap<String, BackupInfo>();
 			write_backup_map();
 		}
+		List<String> on_disk_ids = pp.getBackupIdentifiers();
+		Map<String,BackupInfo> add = new HashMap<String,BackupInfo>();
+		INFO("ON DISK");
+		for (String s : on_disk_ids)
+		{
+			BackupInfo b = new BackupInfo(s);
+			boolean in_map = last_backup_map.containsKey(b.id());
+			INFO(s+" in map="+in_map);
+			if (!in_map)
+				add.put(b.id(), b);
+		}
+		INFO("IN MAP");
+		List<String> remove = new ArrayList<String>();
+		for (String k : last_backup_map.keySet())
+		{
+			if (last_backup_map.get(k) instanceof BackupInfo)
+			{
+				BackupInfo b = last_backup_map.get(k);
+				boolean on_disk = on_disk_ids.contains(b.path);
+				INFO(k+" on disk="+on_disk);
+				if (!on_disk)
+					remove.add(k);
+			}
+			else
+			{
+				remove.add(k);
+				INFO("?old map value?"+k+"="+last_backup_map.get(k));
+			}
+		}
+		if (!remove.isEmpty() || !add.isEmpty())
+		{
+			for (String k : remove)
+				last_backup_map.remove(k);
+			for (String k : add.keySet())
+				last_backup_map.put(add.get(k).id(),add.get(k));
+			write_backup_map();
+		}
+		INFO("BACKUP MAP INIT COMPLETE");
 	}
 	
-	private void write_backup_map()throws Exception
+	private void remove_old_backups() throws PersistenceException, IOException
+	{
+		int c = 0;
+		int s = last_backup_map.size() - max_days ;
+		if (s<1)
+			return;
+		List<String> delete_list = new ArrayList<String>();
+		for (String k : last_backup_map.keySet())
+		{
+			if (c < s)
+				delete_list.add(k);
+			c++;
+		}
+		for (String k : delete_list)
+			delete_backup_by_id(k);
+	}
+	
+	private void delete_backup_by_id(String id) throws PersistenceException, IOException
+	{
+		INFO("DELETING BACKUP "+id);
+		
+		pp.deleteBackup(last_backup_map.get(id).path);
+		last_backup_map.remove(id);
+		write_backup_map();
+		
+	}
+	
+	private void delete_backup_by_path(String path) throws PersistenceException, IOException
+	{
+		BackupInfo b = new BackupInfo(path);
+		INFO("DELETING BACKUP "+b.id());
+		
+		pp.deleteBackup(path);
+		last_backup_map.remove(b.id());
+		write_backup_map();
+		
+	}
+
+	private void write_backup_map() throws IOException
 	{
 		FileOutputStream fos = new FileOutputStream(data_file);
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -90,7 +190,7 @@ public class PersistenceBackupManagerRawUI extends RawUIModule
 	{
 		 ObjectInputStream ois = new ObjectInputStream(new FileInputStream(data_file));
 		 Object o = ois.readObject();
-		 last_backup_map = (HashMap<String,String>) o;
+		 last_backup_map = (TreeMap<String,BackupInfo>) o;
 	}
 	
 	protected void defineSlots()
@@ -123,7 +223,8 @@ public class PersistenceBackupManagerRawUI extends RawUIModule
 		if(params.get("do_full_backup") != null)
 		{
 			String id = pp.doFullBackup();
-			last_backup_map.put(id, new Date().toString());
+			BackupInfo b = new BackupInfo(id);
+			last_backup_map.put(b.id(), b);
 			write_backup_map();
 			DOCUMENT_START(uctx, getName(), RAW_UI_BACKGROUND_COLOR, RAW_UI_FONT_FAMILY, RAW_UI_FONT_COLOR, RAW_UI_FONT_SIZE,RAW_UI_LINK_COLOR,RAW_UI_LINK_HOVER_COLOR);
 			P(uctx);
@@ -143,10 +244,8 @@ public class PersistenceBackupManagerRawUI extends RawUIModule
 		}
 		else if(params.get("do_delete") != null)
 		{
-			String id = (String)params.get("id");
-			pp.deleteBackup(id);
-			last_backup_map.remove(id);
-			write_backup_map();
+			String path = (String)params.get("id");
+			delete_backup_by_path(path);
 			DOCUMENT_START(uctx, getName(), RAW_UI_BACKGROUND_COLOR, RAW_UI_FONT_FAMILY, RAW_UI_FONT_COLOR, RAW_UI_FONT_SIZE,RAW_UI_LINK_COLOR,RAW_UI_LINK_HOVER_COLOR);
 			P(uctx);
 			SPAN(uctx,pp.getName()+" DELETED BACKUP OK",18);
@@ -211,9 +310,7 @@ public class PersistenceBackupManagerRawUI extends RawUIModule
 			for(int i = 0;i < backup_identifiers.size();i++ )
 			{
 				String identifier = backup_identifiers.get(i);
-				String last_backup_date = last_backup_map.get(identifier);
-				if(last_backup_date == null)
-					last_backup_date = "";
+				BackupInfo last_backup_date = last_backup_map.get(identifier);
 				String short_identifier=null;
 				if(identifier.lastIndexOf('\\') != -1)
 					short_identifier = identifier.substring(identifier.lastIndexOf('\\')+1);
@@ -224,7 +321,7 @@ public class PersistenceBackupManagerRawUI extends RawUIModule
 						SPAN(uctx, short_identifier);
 					TD_END(uctx);
 					TD_START(uctx);
-						SPAN(uctx, last_backup_date);
+						SPAN(uctx, last_backup_date.date.toString());
 					TD_END(uctx);
 					TD_START(uctx);
 						A_GET(uctx,getName(),RAW_SUBMODE_DEFAULT,"[ download ]","do_download",true,"id",identifier);
@@ -265,40 +362,30 @@ public class PersistenceBackupManagerRawUI extends RawUIModule
 					
 					
 					backup_thread_is_running = true;
-					Calendar now 	= Calendar.getInstance();
-					int now_hr  	= now.get(Calendar.HOUR_OF_DAY);
-					int now_min 	= now.get(Calendar.MINUTE);
-					int year 		= now.get(Calendar.YEAR);
-					int day_of_year = now.get(Calendar.DAY_OF_YEAR);
-					String backup_for_day_key = String.valueOf(year)+"_"+String.valueOf(day_of_year);
-					full_backup_for_day_is_done = last_backup_map.get(backup_for_day_key)!=null;
-					
+					BackupInfo b = new BackupInfo();
+
+					int now_hr  	= b.date.get(Calendar.HOUR_OF_DAY);
+					int now_min 	= b.date.get(Calendar.MINUTE);
+					full_backup_for_day_is_done = last_backup_map.get(b.id())!=null;
 					
 					if(now_hr >= backup_time_hr && now_min >= backup_time_min && !full_backup_for_day_is_done)
 					{
 						try{
 							String id = pp.doFullBackup();
 							full_backup_for_day_is_done = true;
-							last_backup_map.put(id,new Date().toString());
-							last_backup_map.put(backup_for_day_key,"true");
-							//delete the previous days keys so we dont bloat the hashmap//
-							for(int i = day_of_year-1;i >= 0;i--)
-							{
-								String delete_key =  String.valueOf(year)+"_"+String.valueOf(i);
-								last_backup_map.remove(delete_key);
-							}
-							if(day_of_year == 1)
-							{
-								String delete_key =  String.valueOf(year-1)+"_"+String.valueOf(365);
-								last_backup_map.remove(delete_key);	
-								delete_key =  String.valueOf(year-1)+"_"+String.valueOf(366);
-								last_backup_map.remove(delete_key);	
-							}
-
+							b = new BackupInfo(id);
+							last_backup_map.put(b.id(), b);
 							write_backup_map();
 						}catch(Exception pe)
 						{
 							pe.printStackTrace();
+						}
+						try
+						{
+							remove_old_backups();
+						} catch (Exception e)
+						{
+							ERROR("Couldnt remove old backup",e);
 						}
 					}
 					
@@ -321,5 +408,60 @@ public class PersistenceBackupManagerRawUI extends RawUIModule
 		
 	}
 	
+	class BackupInfo implements Serializable {
+
+		private static final long serialVersionUID = -5810397801922315766L;
+		public Calendar date;
+		public String path;
+		public BackupInfo()
+		{
+			this.date = Calendar.getInstance();
+		}
+		
+		public BackupInfo(String path)
+		{
+			this.path = path;
+			this.date = Calendar.getInstance();
+			get_date_from_path();
+		}
+		
+		private void get_date_from_path()
+		{
+			String[] parts = path.split(File.separator);
+			String dir_name = parts[parts.length-1];
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd_HHmmss"); // from bdbstore dobackup
+		    Date d;
+			try
+			{
+				d = formatter.parse(dir_name);
+			    this.date.setTime(d);
+			} catch (ParseException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		public String id()
+		{
+//			int now_hr  	= date.get(Calendar.HOUR_OF_DAY);
+//			int now_min 	= date.get(Calendar.MINUTE);
+			int year 		= date.get(Calendar.YEAR);
+			int day_of_year = date.get(Calendar.DAY_OF_YEAR);
+			return year+"_"+day_of_year;
+		}
+		
+		private void writeObject(ObjectOutputStream out) throws IOException
+	   {
+	     out.writeObject(date);
+	     out.writeObject(path);
+	   }
+	   private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException
+	   {
+	    this.date = (Calendar) in.readObject();
+	    this.path = (String) in.readObject();
+	   }
+		
+		
+	}
 	
 }
