3b3152cdd6163f2286e003bf5b535345c610d953
topher <topher@241bf0be-db3f-0410-a781-e906b5fc445c>
2011-11-04 17:42:53 +0000

    git-svn-id: http://posttool.com/svn/posttool/PSWebV1.0@7714 241bf0be-db3f-0410-a781-e906b5fc445c

--- src/com/pagesociety/web/module/registration/RestrictListModule.java
+++ src/com/pagesociety/web/module/registration/RestrictListModule.java
@@ -4,13 +4,19 @@ package com.pagesociety.web.module.registration;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import javax.servlet.http.HttpServletResponse;
+
+import com.pagesociety.web.UserApplicationContext;
 import com.pagesociety.web.WebApplication;
 import com.pagesociety.web.exception.InitializationException;
 import com.pagesociety.web.exception.WebApplicationException;
+import com.pagesociety.web.gateway.RawCommunique;
+import com.pagesociety.web.module.Export;
 import com.pagesociety.web.module.WebModule;
 
 public class RestrictListModule extends WebModule
@@ -42,9 +48,29 @@ public class RestrictListModule extends WebModule
 		}
 	}
 
+	@Export
+	public void Hup(UserApplicationContext uctx,RawCommunique r)
+	{
+		HttpServletResponse resp = (HttpServletResponse)r.getResponse();
+
+		try{
+			init(getApplication(), getConfig());
+			resp.getWriter().println("<h1>Success - reloaded "+restrict_list_filename+"</h1>");
+		}catch(Exception e)
+		{
+			try{
+				resp.getWriter().println("<h1>Failed - See Logs</h1>");
+			}catch(IOException ioe)
+			{
+				ioe.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+	}
 	
 	public void load_restrict_list() throws WebApplicationException
 	{
+		INFO("RELOADING RESTRICT LIST");
 		File rl = new File(restrict_list_filename);
 		current_resrict_list_length = rl.length();
 		allowed_email_addresses.clear();
@@ -57,6 +83,7 @@ public class RestrictListModule extends WebModule
 			{
 				if(line.trim().equals(""))
 					continue;
+				INFO("ADDING... "+line.toLowerCase());
 				if(line.startsWith("*"))
 					allowed_domains.add(line.toLowerCase().substring(line.indexOf("@")));
 				else
